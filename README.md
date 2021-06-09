ad04d8efe8a75f3874cff690ff908e870baef3d1
DucaSP <ducasp@gmail.com>
2021-06-09 10:43:40 -0300

    Changing comments/headers to notice about the license model as this code is now open source

--- README.md
+++ README.md
@@ -1,5 +1,46 @@
 # ESP8266 UNAPI Firmware
 
-It depends on ESP8266 Arduino IDE customizations available at:
+Two sample TCP IP UNAPI drivers that work along with this firmware
+are available at:
+
+(ROM Version)
+https://github.com/ducasp/MSX-Development/tree/master/MSX-SM/WiFi/UNAPI_BIOS_CUSTOM_ESP_FIRMWARE
+
+(RAM Version, requires Memory Mapper and RAMHELPR by Konamiman)
+https://github.com/ducasp/MSX-Development/tree/master/MSX-SM/WiFi/UNAPI_DRIVER_CUSTOM_ESP_FIRMWARE
+
+Project Design Constraints:
+
+This firmware was designed taking into consideration that the other end connected to it either has
+a large 2KB reception buffer OR is fast enough to pick-up data at the desired baud rate without the
+help of such large buffer.
+
+Copyright (c) 2019 - 2021 Oduvaldo Pavan Junior ( ducasp@ gmail.com )
+All rights reserved.
+
+If you integrate this on your hardware, please consider the possibility of sending one piece of it
+as a thank you to the author :) Of course this is not mandatory, if you like the idea, contact the
+author in the e-mail address above.
+
+This software uses ESP8266 for Arduino IDE library, licensed under
+GNU Lesser General Public License 2.1 and available at:
+
+https://github.com/esp8266/Arduino
+
+It depends on some customizations I've made on that library, those
+are available at:
 
 https://github.com/ducasp/ArduinoIDE_ESP8266_Customizations
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as 
+published by the Free Software Foundation, either version 2.1 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
--- UNAPI8266.h
+++ UNAPI8266.h
@@ -1,37 +1,31 @@
 /*
---
--- UNAPI8266.h
---   ESP8266 UNAPI Implementation.
---   Revision 1.20
---
--- Requires Arduino IDE and ESP8266 libraries
--- Copyright (c) 2019 - 2020 Oduvaldo Pavan Junior ( ducasp@ gmail.com )
--- All rights reserved.
---
--- Redistribution and use of this source code or any derivative works, are
--- permitted provided that the following conditions are met:
---
--- 1. Redistributions of source code must retain the above copyright notice,
---    this list of conditions and the following disclaimer.
--- 2. Redistributions in binary form must reproduce the above copyright
---    notice, this list of conditions and the following disclaimer in the
---    documentation and/or other materials provided with the distribution.
--- 3. Redistributions may not be sold, nor may they be used in a commercial
---    product or activity without specific prior written permission.
--- 4. Source code of derivative works MUST be published to the public.
---
--- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
--- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
--- TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
--- PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
--- CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
--- EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
--- PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
--- OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
--- WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
--- OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
--- ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
---
+UNAPI8266.h
+    ESP8266 UNAPI Implementation.
+    Revision 1.20
+
+Requires Arduino IDE and ESP8266 libraries
+
+Copyright (c) 2019 - 2021 Oduvaldo Pavan Junior ( ducasp@ gmail.com )
+All rights reserved.
+
+If you integrate this on your hardware, please consider the 
+possibility of sending one piece of it as a thank you to the author :)
+Of course this is not mandatory, if you like the idea, contact the
+author in the e-mail address above.
+
+This file, that is part of ESP8266 UNAPI Firmware program, is free
+software: you can redistribute it and/or modify it under the terms
+of the GNU Lesser General Public License as published by the Free
+Software Foundation, either version 2.1 of the License, or (at your
+option) any later version.
+
+This file is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this file.  If not, see <https://www.gnu.org/licenses/>
 */
 
 #ifndef _UNAPI8266_H
